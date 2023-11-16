e1666f5513311f04960772fa3c3193c61acffa03
kite707 <12201680@inha.edu>
2023-11-16 22:18:32 +0900

    test: 실패 테스트케이스 추가

--- test/avlTreeTest.cc
+++ test/avlTreeTest.cc
@@ -1,5 +1,7 @@
 #include "../header/avlTree.h"
 #include <gtest/gtest.h>
 
-TEST(AVL, avlTree_Test) { EXPECT_EQ(3, add(1, 2)); }
-
+TEST(AVL, avlTree_Test) {
+  EXPECT_EQ(3, add(1, 2));
+  EXPECT_EQ(4, add(1, 2));
+}
