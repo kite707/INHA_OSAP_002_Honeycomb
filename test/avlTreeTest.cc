0096906c9a57d1507ba2cd8597c00a8b7af9d38d
kite707 <12201680@inha.edu>
2023-11-16 23:31:51 +0900

    style: .clang-format 파일 추가

--- test/avlTreeTest.cc
+++ test/avlTreeTest.cc
@@ -1,5 +1,5 @@
-#include "../header/avlTree.h"
 #include <gtest/gtest.h>
+#include "../header/avlTree.h"
 
 TEST(AVL, avlTree_Test) {
   EXPECT_EQ(3, add(1, 2));
--- test/mainTest.cc
+++ test/mainTest.cc
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(MAIN, mainTest) { EXPECT_EQ(7 * 6, 42); }
+TEST(MAIN, mainTest) {
+  EXPECT_EQ(7 * 6, 42);
+}
