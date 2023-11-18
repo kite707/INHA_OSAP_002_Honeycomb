5544b30da4ae8408a69b5e850e6846a2f129b48b
abyss-s <lh3729@naver.com>
2023-11-19 03:49:12 +0900

    feat: 생성자 루트 노드 변수 초기화

--- source/bstTree.cc
+++ source/bstTree.cc
@@ -1,10 +1,13 @@
 #include "../header/bstTree.h"
-
 using namespace std;
+
+// 스켈레톤 코드
 int multiple(int a, int b) {
-  return a * b;
+	return a * b;
 }
 
 /* 생성자 */
 template <typename T>
-BinarySearchTree<T>::BinarySearchTree() {}
+BinarySearchTree<T>::BinarySearchTree(){
+	root = nullptr; 
+};
