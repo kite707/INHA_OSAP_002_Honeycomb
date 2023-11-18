3f8ab3af1446055161bfaebb41252ecc93d36c8b
abyss-s <lh3729@naver.com>
2023-11-19 03:26:50 +0900

    feat: 멤버 함수 제외한 헤더 파일 구조 구현

--- header/bstTree.h
+++ header/bstTree.h
@@ -1,4 +1,29 @@
 #ifndef BSTTREE_H_
 #define BSTTREE_H_
-int multiple(int a, int b);
-#endif
+
+int multiple(int a, int b); // 스켈레톤 코드
+
+/* Node 구조체 */
+template <typename T>
+struct Node {
+  T key;
+  Node* left;
+  Node* right;
+  int height;
+};
+
+/* BST 클래스 */
+template <typename T>
+class BinarySearchTree {
+ public:
+  BinarySearchTree();
+  // 멤버 함수 구현 필요
+ private:
+  typedef Node<T>* NodePtr;
+  NodePtr root;
+};
+#endif  // BSTTREE_H_
+
+
+
+
