a9139b80c300f292c5f26ce2ce6a1949a848015e
abyss-s <lh3729@naver.com>
2023-11-21 03:47:29 +0900

    feat: 테스트를 위한 픽스처 클래스 구현

--- test/bstTreeTest.cc
+++ test/bstTreeTest.cc
@@ -1,5 +1,24 @@
 #include <gtest/gtest.h>
+#include <iostream>
 #include "../header/bstTree.h"
+#include "../source/bstTree.cc"
+using namespace testing;
+using namespace std;
+
+// Fixture Class
+template <typename T>
+class BinarySearchTreeTest : public Test {
+ protected:
+  void SetUp() override {
+    // Init for testing
+  }
+
+  void TearDown() override {
+    // Clean up
+  }
+
+  BinarySearchTree<T> bst;  // BST object
+};
 
 /*
 TEST(BST, bstTree_Test) {
