8ea64a4308429409f37cd76ff9abceaf17ac9b19
kite707 <12201680@inha.edu>
2023-11-21 20:41:12 +0900

    feat: avlTree Insert 예시 코드

--- header/avl_tree.h
+++ header/avl_tree.h
@@ -1,4 +1,21 @@
 #ifndef AVLTREE_H_
 #define AVLTREE_H_
-int add(int a, int b);
+
+#include <iostream>
+#include "bst_tree.h"
+
+/* AVL Tree Class */
+template <typename T>
+class AVLTree : public BinarySearchTree<T> {
+ public:
+  AVLTree();
+  NodePtr<T> Insert(NodePtr<T>& r, int item);
+  int getBalanceFactor(NodePtr<T>& r);
+  // NodePtr RotateLeft(NodePtr<T>& x);
+  // NodePtr RotateRight(NodePtr<T>& y);
+  // void Balancing(NodePtr<T>& r, int item);
+
+ private:
+};
+
 #endif
--- source/avl_tree.cc
+++ source/avl_tree.cc
@@ -1,6 +1,19 @@
 #include "../header/avl_tree.h"
+#include "../header/bst_tree.h"
 
 using namespace std;
-int add(int a, int b) {
-  return a + b;
+
+// 생성자의 정의
+template <typename T>
+AVLTree<T>::AVLTree() : BinarySearchTree<T>() {
+  std::cout << "avl tree 생성자 호출" << std::endl;
+}
+
+// avlMemberGetter 함수의 정의
+template <typename T>
+NodePtr<T> AVLTree<T>::Insert(NodePtr<T>& r, int item) {
+  NodePtr<T> z = new Node<T>;
+  z->key = item;
+  r = z;
+  return r;
 }
