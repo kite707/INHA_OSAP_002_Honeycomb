a70203f350e456a5adcde71b10d29f4dcbe1fa8b
kite707 <12201680@inha.edu>
2023-11-24 20:53:13 +0900

    test: avlTree 생성자 테스트코드 작성

--- test/avl_tree_test.cc
+++ test/avl_tree_test.cc
@@ -1,6 +1,14 @@
 ﻿#include "../header/avl_tree.h"
 #include <gtest/gtest.h>
+#include "../header/bst_tree.h"
+#include "../source/avl_tree.cc"
+#include "../source/bst_tree.cc"
 
-TEST(AVL, avlTree_Test) {
-  EXPECT_EQ(2 * 3, 6);
+TEST(AVLTreeTest, ConstructorTest) {
+  AVLTree<int> avl;
+
+  int size = avl.getSize();
+
+  ASSERT_EQ(0, size);
+  ASSERT_EQ(nullptr, avl.getRoot());
 }
