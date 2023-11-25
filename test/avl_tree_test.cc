357effe9c39dce9d08efd06e2d883ae0a9cc39c2
kite707 <12201680@inha.edu>
2023-11-25 20:58:39 +0900

    test: insert, recursiveInsert, getBalanceFactor 테스트코드 추가

--- test/avl_tree_test.cc
+++ test/avl_tree_test.cc
@@ -3,12 +3,11 @@
 #include "../header/bst_tree.h"
 #include "../source/avl_tree.cc"
 #include "../source/bst_tree.cc"
-  // SetUp함수 실행 후 테스트 초기 상태
-  //   2
-  //  / \
-  // 1   4
-  //    / \
-  //   3   5
+// SetUp함수 실행 후 테스트 초기 상태
+//   1
+//    \
+//     2
+
 TEST(AVLTreeTest, ConstructorTest) {
   AVLTree<int> avl;
 
@@ -25,6 +24,11 @@ class AvlTreeTestFixture : public testing::Test {
   void TearDown() override;
 
  protected:
+  int getBalanceFactorWrapper(NodePtr<int> current_node) {
+    return avl_.getBalanceFactor(current_node);
+  }
+
+ protected:
   AVLTree<int> avl_;
 };
 AvlTreeTestFixture::AvlTreeTestFixture() {
@@ -36,25 +40,42 @@ AvlTreeTestFixture::~AvlTreeTestFixture() {
 }
 
 void AvlTreeTestFixture::SetUp() {
-  std::cout << "[SetUp] 1~5삽입\n";
-  avl_.insert(avl_.getRoot(), 1);
-  avl_.insert(avl_.getRoot(), 2);
-  avl_.insert(avl_.getRoot(), 3);
-  avl_.insert(avl_.getRoot(), 4);
-  avl_.insert(avl_.getRoot(), 5);
+  std::cout << "[SetUp] 1~2삽입\n";
+  avl_.insert(1);
+  avl_.insert(2);
 }
 void AvlTreeTestFixture::TearDown() {
   // std::cout << "TearDown called\n";
 }
 
-TEST_F(AvlTreeTestFixture, insertTest) {
+// 현재 상태(1~3 삽입)
+//   2
+//  / \
+// 1   3
+TEST_F(AvlTreeTestFixture, recursiveInsertTest) {
+  this->avl_.recursiveInsert(avl_.getRoot(), 3);
+  ASSERT_EQ(1, this->avl_.findDepthByValue(3));
+}
+
+// 현재 상태(1~3 삽입)
+//   2
+//  / \
+// 1   3
+TEST_F(AvlTreeTestFixture, insertTest_General) {
+  this->avl_.insert(3);
   ASSERT_EQ(1, this->avl_.findDepthByValue(1));
   ASSERT_EQ(0, this->avl_.findDepthByValue(2));
-  ASSERT_EQ(2, this->avl_.findDepthByValue(3));
-  ASSERT_EQ(1, this->avl_.findDepthByValue(4));
-  ASSERT_EQ(2, this->avl_.findDepthByValue(5));
+  ASSERT_EQ(1, this->avl_.findDepthByValue(3));
 }
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+
+// 현재 상태(1~4 삽입)
+//   2
+//  / \
+// 1   3
+//      \
+//       4
+TEST_F(AvlTreeTestFixture, getBalanceFactorTest) {
+  this->avl_.insert(3);
+  this->avl_.insert(4);
+  ASSERT_EQ(-1, this->avl_.getBalanceFactor(this->avl_.getRoot()));
 }
